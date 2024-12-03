641179c0d0e38244ee5fc91e741b8f1422b2c094
deokudev <sddev1002@gmail.com>
2024-12-03 21:04:41 +0900

    ionic start 완료

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'life-of-hwaksin-memory',
+  webDir: 'www'
+};
+
+export default config;
